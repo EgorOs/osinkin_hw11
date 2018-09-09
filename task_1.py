4a7b71ce62e9db274a5c9d8de9afeaea6b85dac7
Egor Osinkin <egoros97@yandex.ru>
2018-09-09 17:38:58 +0300

    Subplot added

--- task_1.py
+++ task_1.py
@@ -6,13 +6,14 @@ from time import time
 import matplotlib.pyplot as plt
 import math
 
-def create_dataset(name, init_size=10, max_size=100, step=1, fill_value=1, ntests=1):
-    h = max_size//step
-    data = np.full((h+1, 1 + ntests), np.nan)
+def create_dataset(name, fill_value=1, ntests=1):
     pos = 0
-    for i in range(init_size, max_size + step, step):
+    log_space = sorted({int(i) for i in np.logspace(0, 3.2)} - {1})
+    data = np.full((len(log_space)+1, 1 + ntests), np.nan)
+    for i in log_space:
         data[pos, 0] = i
         for t in range(1, ntests + 1):
+            print('Size', i, 'Test:', t)
             m = np.full((i, i), fill_value)
             start_time = time()
             m = np.dot(m, m)
@@ -25,38 +26,47 @@ def create_dataset(name, init_size=10, max_size=100, step=1, fill_value=1, ntest
     with pd.ExcelWriter (name) as xlsx:
         df.to_excel(xlsx)
 
-def sqr_error(data, mean):
-    standart_deviation = np.empty(mean.size)
-    data_matrix = data.values
-    for i in range(1, len(standart_deviation)):
-        standart_deviation[i] = (sum([(d - mean[i])**2 for d in data_matrix[i]])/(len(data_matrix[0]) - 1))**0.5
-
-    return standart_deviation
-
-s = pd.DataFrame()
-# create_dataset('data.xlsx', init_size=0, max_size=1500, step=50, ntests=5)
+# create_dataset('data.xlsx', ntests=7)
 
 df = pd.read_excel('data.xlsx')
 # List of columns with time data
 time = [t for t in df.keys() if 'Time' in t]
 mean_by_row = df[time].mean(axis=1)
-error = sqr_error(df[time], mean_by_row)
+error = df[time].std(axis=1)
 
-T = mean_by_row[1::]
-N = df['Size'].values[1::]
+T = mean_by_row
+N = df['Size'].values
 log_N = np.log(N)
 log_T = np.log(T)
 k, b = np.polyfit(log_N, log_T, 1)
-
+print('n =', k)
 
 # line fitting explained here
 # https://stackoverflow.com/questions/30657453/fitting-a-straight-line-to-a-log-log-curve-in-matplotlib
 # plotting examples https://matplotlib.org/examples/pylab_examples/log_demo.html
 
-# Initialize empty plot in log scale
-plt.loglog()
+# Initialize empty figure with fixated size
+plt.figure(figsize=(14, 10))
+plt.subplots_adjust(hspace=0.3, wspace=0.3)
+
+# subplot(n-rows, n-columns, position)
+plt.subplot(1,2,1)
+
 # Line approximation
-plt.loglog(N, np.exp(log_N*k + b))
-plt.errorbar(N, T, error[1::], None, 'o', markersize=3)
+approx, *__ = plt.loglog(N, np.exp(log_N*k + b), 'k-')
+err, *__ = plt.errorbar(N, T, yerr=error, fmt='o', markersize=2)
+
+plt.xlabel('$log(N)$')
+plt.ylabel('$log(T)$')
+plt.title('$Multiplication$ $time$ $from$ $matrix$ $size$')
+plt.legend(['$Approximation$', '$Mean$ $and$ $deviation$'])
 plt.grid(True)
+
+
+plt.subplot(1,2,2)
+
+
+plt.xlabel('$N$')
+plt.ylabel('$n(N)$')
+plt.title('$Correlation$ $of$ $complexity$ $and$ $size$')
 plt.show()
