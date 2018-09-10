d378f37e60f63e35667bf49de93c55a7583f06e4
Egor Osinkin <egoros97@yandex.ru>
2018-09-10 23:54:31 +0300

    Code cleaned, outliers filtering added

--- task_1.py
+++ task_1.py
@@ -5,10 +5,12 @@ import pandas as pd
 from time import time
 import matplotlib.pyplot as plt
 import math
+import os
 
-def create_dataset_logspace(name, fill_value=1, ntests=1):
+
+def create_dataset_logspace(name, min_pwr=0, max_pwr=3.2, fill_value=1, ntests=1):
     pos = 0
-    log_space = sorted({int(i) for i in np.logspace(0, 3.2)} - {1})
+    log_space = sorted({int(i) for i in np.logspace(min_pwr, max_pwr)} - {1})
     data = np.full((len(log_space)+1, 1 + ntests), np.nan)
     for i in log_space:
         data[pos, 0] = i
@@ -26,9 +28,10 @@ def create_dataset_logspace(name, fill_value=1, ntests=1):
     with pd.ExcelWriter (name) as xlsx:
         df.to_excel(xlsx)
 
-def create_dataset_linspace(name, fill_value=1, ntests=1):
+
+def create_dataset_linspace(name, st, end, num, fill_value=1, ntests=1):
     pos = 0
-    log_space = sorted({int(i) for i in np.linspace(2, 500, 250)} - {1})
+    log_space = sorted({int(i) for i in np.linspace(st, end, num)} - {1})
     data = np.full((len(log_space)+1, 1 + ntests), np.nan)
     for i in log_space:
         data[pos, 0] = i
@@ -47,9 +50,20 @@ def create_dataset_linspace(name, fill_value=1, ntests=1):
     with pd.ExcelWriter (name) as xlsx:
         df.to_excel(xlsx)
 
-# create_dataset_logspace('data_3_tests.xlsx', ntests=3)
 
-df = pd.read_excel('data_3_tests.xlsx')
+PLOT_1_DATASET_NAME = 'plot_1_data.xlsx'
+plot_1_dataset_params = {
+                        'name': PLOT_1_DATASET_NAME,
+                        'min_pwr': 0,
+                        'max_pwr': 3.2,
+                        'fill_value': 5,
+                        'ntests': 3,
+                        }
+# Created dataset if it doesn't exist
+if PLOT_1_DATASET_NAME not in os.listdir():
+    create_dataset_logspace(**plot_1_dataset_params)
+
+df = pd.read_excel(PLOT_1_DATASET_NAME)
 # List of columns with time data
 time_cols = [t for t in df.keys() if 'Time' in t]
 mean_by_row = df[time_cols].mean(axis=1)
@@ -83,9 +97,22 @@ plt.legend(['$Approximation$', '$Mean$ $and$ $deviation$'])
 plt.grid(True)
 
 
-# create_dataset_linspace('data_lin_5_tests.xlsx', ntests=5)
+PLOT_2_DATASET_NAME = 'plot_2_data.xlsx'
+plot_2_dataset_params = {
+                        'name': PLOT_2_DATASET_NAME,
+                        'st': 2,
+                        'end': 500,
+                        'num': 400,
+                        'fill_value': 5,
+                        'ntests': 25,
+                        }
+# Created dataset if it doesn't exist
+if PLOT_2_DATASET_NAME not in os.listdir():
+    create_dataset_linspace(**plot_2_dataset_params)
 
-df = pd.read_excel('data_lin_5_tests.xlsx')
+
+df = pd.read_excel(PLOT_2_DATASET_NAME)
+
 # List of columns with time data
 time_cols = [t for t in df.keys() if 'Time' in t]
 mean_by_row = df[time_cols].mean(axis=1)
@@ -94,39 +121,42 @@ error = df[time_cols].std(axis=1)
 T = mean_by_row.values
 N = df['Size'].values
 
+
+# Filter time measurement outliers by relative error
 rel_error = error / T * 100
 REL_ERROR_THRESHOLD = 10 # %
-non_zero = rel_error != 0
-error_mask = rel_error < REL_ERROR_THRESHOLD
-error_mask = error_mask & non_zero
-
-# Filter obvious outliers
+error_mask = (rel_error < REL_ERROR_THRESHOLD) & (rel_error != 0)
 T_inliers = T[error_mask]
 N_inliers = N[error_mask]
 error_inliers = error[error_mask]
 
-
-N2 = np.array(N_inliers[1::])
+# Shift values
 N1 = np.array(N_inliers[:len(N_inliers) - 1]) 
-T2 = np.array(T_inliers[1::])
+N2 = np.array(N_inliers[1::])
 T1 = np.array(T_inliers[:len(T_inliers) - 1])
-T2_err = np.array(error_inliers[1::])
+T2 = np.array(T_inliers[1::])
 T1_err = np.array(error_inliers[:len(error_inliers) - 1])
+T2_err = np.array(error_inliers[1::])
 
+# Calculate complexity 
 n = np.abs(np.log(T2/T1)/np.log(N2/N1))
-n_err = np.exp(np.log(T2_err/T1_err))
-n_rel_err = abs(n_err/n*100)
+n_abs_err = np.log(T2_err/T1_err)
+# n_abs_err = np.log((T2_err/T2)/(T1_err/T2))*n
+n_rel_err = abs(n_abs_err/n*100)
 
-mask = (n_rel_err < 50) & (n < 4)
+# Set filtering parameters
+mask = (n_rel_err < 20) & (n < 3.5) & (n > 2)
 k, b = np.polyfit(N1[mask], n[mask], 1)
 
-
+# Check log err bars https://faculty.washington.edu/stuve/log_error.pdf
+# shall try calculate n using not mean T, but each column instead
 plt.subplot(1,2,2)
-err, *__ = plt.errorbar(N1[mask], n[mask], yerr=n_err[mask], fmt='o', markersize=2)
+plt.ylim((0, 4))
+err, *__ = plt.errorbar(N1[mask], n[mask], yerr=n_abs_err[mask], fmt='o', markersize=2)
 approx, *__ = plt.plot(N1[mask], N1[mask]*k + b, 'k-')
 plt.xlabel('$N$')
 plt.ylabel('$n(N)$')
 plt.title('$Correlation$ $of$ $complexity$ $and$ $size$')
-plt.legend(['$Approximation$', '$Mean$ $and$ $deviation$'], loc=2)
+plt.legend(['$Approximation$', '$Mean$ $and$ $deviation$'], loc=4)
 plt.grid(True)
 plt.show()
